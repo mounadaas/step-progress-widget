cd2edf0fbb76e12f989d3acf8f0b4ae859e7c97f
mounadaas <mounadaas19@gmail.com>
2024-03-21 20:25:10 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
+import {useState} from 'react'
 import './App.css';
+const messages=[
+  "Learn Raect",
+  "Apply for job",
+  "Invest your new Income"
+]
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [step, setStep] = useState(1);
+  let [isOpen, setIsopen] = useState(true)
+
+  function handelNext(){
+    if(step < 3 ){   
+        setStep(s => s + 1)
+    }   
+  }
+  function handelPrevious(){
+    if(step > 1 ){
+    setStep( s => s - 1)
+    }
+  }
+  
+  
+
+  return (  
+    <>
+        <button className='btn' onClick={()=>setIsopen(!isOpen)}>&times;</button>
+        {isOpen && (
+
+          <div className="steps">
+              <div className='numbers'>
+                  <div className={ step >= 1 ? 'active' : ""}>1</div>
+                  <div className={ step >= 2 ? 'active' : ""}>2</div>
+                  <div className={ step >= 3 ? 'active' : ""}>3</div>
+              </div>
+              <p className='message'>step {step}: { messages[step - 1]}</p>
+              <div className='buttons'>
+                <button onClick={handelPrevious}>Previous</button>
+                <button onClick={ handelNext}>Next</button>
+  
+              </div>
+          </div>
+        )}
+    
+    </>
   );
 }
 
